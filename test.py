8749c75c4121a13c8d0040c1b0220759077a40f8
farhanc007 <farhan.amin1976@hotmail.com>
2023-11-14 23:39:29 +0000

    editing the python file

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print("hello world")
+x = float(input("Enter a number for x: "))
+y = float(input("Enter a number for y: "))
+if x == y:
+    print("x and y are equal")
+elif x < y:
+    print("x is smaller")
+else:
+    print("y is smaller")
+print("thanks")
